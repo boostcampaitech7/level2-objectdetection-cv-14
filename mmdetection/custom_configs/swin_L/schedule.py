e94d2272f6a8a9a214bdff76153b1da2934b4121
oweixx <lowzxx@naver.com>
2024-10-03 01:24:48 +0900

    [#23] Feature: Modify Scheduler for Swin Transformer Training

--- mmdetection/custom_configs/swin_L/schedule.py
+++ mmdetection/custom_configs/swin_L/schedule.py
@@ -1,9 +1,8 @@
-# optimizer
 optimizer_config = dict(grad_clip=None)
 
 optimizer = dict(
     type='AdamW',
-    lr=1e-3,
+    lr=1e-4,
     betas=(0.9, 0.999),
     weight_decay=0.05,
     paramwise_cfg=dict(
