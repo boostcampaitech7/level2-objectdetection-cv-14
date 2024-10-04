ccb757649827630a9a80915d6f446e5be46d0e67
lkl4502 <lkl4502@naver.com>
2024-10-04 16:57:22 +0900

    [#25] Fix : Delete dataset.py and Add data-related modifications in base_DETR.py

--- mmdetection/custom_configs/DETR/Base_DETR/base_DETR.py
+++ mmdetection/custom_configs/DETR/Base_DETR/base_DETR.py
@@ -1,3 +1,13 @@
-_base_ = ["dataset.py",
-          ]
+_base_ = "../../../configs/detr/detr_r50_8x2_150e_coco.py"
+
+# data 관련 부분 수정
+data_root = "../../../../dataset"
+
+data = dict(
+    train=dict(
+        ann_file=data_root + 'train.json',
+        img_prefix=data_root),
+    test=dict(
+        ann_file=data_root + 'test.json',
+        img_prefix=data_root))
 
