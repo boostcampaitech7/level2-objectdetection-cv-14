c084150d2872ec363382a39f6048f34dc72377ad
lkl4502 <lkl4502@naver.com>
2024-10-04 17:23:27 +0900

    [#25] Feature : modify base_DETR.py path and model_related modification apply

--- mmdetection/custom_configs/DETR/base_DETR.py
+++ mmdetection/custom_configs/DETR/base_DETR.py
@@ -1,7 +1,7 @@
-_base_ = "../../../configs/detr/detr_r50_8x2_150e_coco.py"
+_base_ = "../../configs/detr/detr_r50_8x2_150e_coco.py"
 
 # data 관련 부분 수정
-data_root = "../../../../dataset"
+data_root = "../../../dataset"
 
 data = dict(
     train=dict(
@@ -11,3 +11,10 @@ data = dict(
         ann_file=data_root + 'test.json',
         img_prefix=data_root))
 
+
+# model 관련 class 개수 수정
+model = dict(
+    bbox_head=dict(
+        num_classes=10
+    )
+)
