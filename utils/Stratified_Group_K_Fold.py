7ff9a5489cc2efe9e62e187b8cc0d7d3046ea6a4
lkl4502 <lkl4502@naver.com>
2024-10-08 05:53:59 +0900

    [#28] Feature : Stratified Group K Fold Function Implement

--- utils/Stratified_Group_K_Fold.py
+++ utils/Stratified_Group_K_Fold.py
@@ -1,3 +1,4 @@
+import os
 import json
 import numpy as np
 import pandas as pd
@@ -23,11 +24,13 @@ def main():
     # 인자 받기
     args = parser.parse_args()
 
+    # train.json 파일 절대 경로
     annotation = "/data/ephemeral/home/dataset/train.json"
 
     with open(annotation) as f: 
         data = json.load(f)
-
+    
+    # annotation 정보에서 image_id와 category_id를 가져와 튜플 구성 후 리스트로 반환
     var = [(ann['image_id'], ann['category_id']) for ann in data['annotations']]
 
     X = np.ones((len(data['annotations']), 1))
@@ -40,19 +43,55 @@ def main():
 
     index = ['training set']
 
-    for fold_ind, (train_idx, val_idx) in enumerate(cv.split(X,y, groups)):
-        train_y, val_y = y[train_idx], y[val_idx]
-        train_gr, val_gr = groups[train_idx], groups[val_idx]
+    for fold_ind, (train_idx, val_idx) in enumerate(cv.split(X, y, groups), start = 1):
+        train_y, val_y = y[train_idx], y[val_idx] 
+        # group에서 중복을 제거하고 뒤에 있을 in 연산자에서 O(1)로 수행하기 위해 set으로 변환
+        train_gr, val_gr = set(groups[train_idx]), set(groups[val_idx]) 
 
-        assert len(set(train_gr) & set(val_gr)) == 0
-        distrs.append(get_distribution(train_y) + [len(np.unique(train_gr))])
+        assert len(train_gr & val_gr) == 0
 
-        distrs.append(get_distribution(val_y) + [len(np.unique(val_gr))])
+        distrs.append(get_distribution(train_y) + [len(train_gr)])
+        distrs.append(get_distribution(val_y) + [len(val_gr)])
+
         index.append(f'train - fold{fold_ind}')
         index.append(f'val - fold{fold_ind}')
 
+        # train group에 해당하는 것만 가져와 list 구성
+        train_imgs = [img for img in data['images'] if img['id'] in train_gr]
+        train_data = {
+            "images" : train_imgs, # train image 정보
+            "categories" : data["categories"], #category 정보
+            # train_idx에 해당하는 annotation들로 list 구성
+            "annotations" : [data['annotations'][idx] for idx in train_idx] 
+        }
+
+        # val group에 해당하는 것만 가져와 list 구성
+        val_imgs = [img for img in data['images'] if img['id'] in val_gr]
+        val_data = {
+            "images" : val_imgs, # val image 정보
+            "categories" : data["categories"], #category 정보
+            # val_idx에 해당하는 annotation들로 list 구성
+            "annotations" : [data['annotations'][idx] for idx in val_idx]
+        }
+
+        # /data/ephemeral/home/dataset/sgkf
+        basic_path = os.path.join(os.path.split(annotation)[0], "sgkf")
+
+        if not os.path.exists(basic_path):
+            os.mkdir(basic_path)
+
+        # /data/ephemeral/home/dataset/sgkf/train_1fold.json
+        train_path = os.path.join(basic_path, f"train_{fold_ind}fold.json")
+        with open(train_path, "w") as f:
+            json.dump(train_data, f, indent=4)
+        
+        # /data/ephemeral/home/dataset/sgkf/val_1fold.json
+        val_path = os.path.join(basic_path, f"val_{fold_ind}fold.json")
+        with open(val_path, "w") as f:
+            json.dump(val_data, f, indent=4)
+
     categories = [d['name'] for d in data['categories']]
     print(pd.DataFrame(distrs, index=index, columns = [categories[i] for i in range(np.max(y) + 1)] + ["image_cnt"]))
-
+    
 if __name__=="__main__":
     main()
