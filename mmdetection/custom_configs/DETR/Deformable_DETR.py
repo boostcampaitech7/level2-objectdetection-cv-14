a87839d8d395bce7f9a2ae799b6e351ae4ee36de
lkl4502 <lkl4502@naver.com>
2024-10-11 00:57:52 +0900

    [#42] Feature : Update Deformable_DETR before apply DINO

--- mmdetection/custom_configs/DETR/Deformable_DETR.py
+++ mmdetection/custom_configs/DETR/Deformable_DETR.py
@@ -15,8 +15,29 @@ data = dict(
         img_prefix=data_root))
 
 model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='SwinTransformer',
+        pretrain_img_size=384,
+        embed_dims=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        window_size=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(1, 2, 3),
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(type='Pretrained', checkpoint='https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth')),
+    neck=dict(in_channels=[384, 768, 1536]),
     bbox_head=dict(
         num_classes=10,
+        in_channels=1536,
         # box refine 기능 추가
         # 모든 디코더 레이어에서 바운딩 박스를 정제하는 기능
         with_box_refine=True,
@@ -55,4 +76,4 @@ lr_config = dict(policy='step',
                  warmup_ratio=0.001,
                  step=5,
                  gamma=0.5)
-runner = dict(type='EpochBasedRunner', max_epochs=30)
+runner = dict(type='EpochBasedRunner', max_epochs=20)
