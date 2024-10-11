c6a3a2480164d6178fc4c666839cbee980f9eafe
kimgeonsu <kundoo0412@gmail.com>
2024-10-11 21:20:21 +0900

    [#35] Fix: train image size = 1024 & device = cuda

--- yolo/train.py
+++ yolo/train.py
@@ -7,8 +7,8 @@ model = YOLO("yolo11n.pt")
 train_results = model.train(
     data="./recycle-trash.yaml",  # path to dataset YAML
     epochs=100,  # number of training epochs
-    imgsz=640,  # training image size
-    device='0',  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
+    imgsz=1024,  # training image size
+    device='cuda',  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
 )
 
 # Evaluate model performance on the validation set
