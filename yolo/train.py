fad8b6adb4a1ab5b7edf35f670c0377a72ba1860
kimgeonsu <kundoo0412@gmail.com>
2024-10-11 14:15:15 +0900

    Fix: fix some address for dataset

--- yolo/train.py
+++ yolo/train.py
@@ -5,7 +5,7 @@ model = YOLO("yolo11n.pt")
 
 # Train the model
 train_results = model.train(
-    data="/data/ephemeral/home/KGS/level2-objectdetection-cv-14/yolo/custom-ultralytics/cfg/datasets/recycle-trash.yaml",  # path to dataset YAML
+    data="./recycle-trash.yaml",  # path to dataset YAML
     epochs=100,  # number of training epochs
     imgsz=640,  # training image size
     device='0',  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
