2f4db943e6aaa7b4431d5686d92b9ed4913c3b1f
kimgeonsu <kundoo0412@gmail.com>
2024-10-11 14:12:21 +0900

    Fix : make dataset fit to yolo's train use sgkf

--- yolo/yolo_dataset_maker.py
+++ yolo/yolo_dataset_maker.py
@@ -1,40 +1,58 @@
 import json
 import os
+import shutil
 
-def convert_coco_to_yolo(coco_json_path, output_dir):
-    # COCO JSON 파일 로드
-    with open(coco_json_path, 'r') as f:
-        coco_data = json.load(f)
-        print(len(coco_data['annotations']))
-        
+origin_image_dir = './datasets/origin_dataset/train'
+
+train_fold_json_path = './datasets/sgkf_5_14/train_1fold.json'
+val_fold_json_path = './datasets/sgkf_5_14/val_1fold.json'
+train_output_dir = './datasets/recycle_trash_dataset/train'
+val_output_dir = './datasets/recycle_trash_dataset/valid'
+
+
+def dataset_maker(read_path, target_path):
+    with open(read_path, 'r') as f:
+        train_ann_data = json.load(f)        
         id = 0
         content = ''
-        for idx, item in enumerate(coco_data['annotations']):
+        for idx, item in enumerate(train_ann_data['annotations']):
             if item['image_id'] != id:
                 filename = f"{id:04d}.txt"
-                output_path = os.path.join(output_dir, filename)
+                output_path = os.path.join(target_path+'/labels', filename)
                 with open(output_path, 'w') as ff:
                     print(output_path)
                     ff.write(content)
+                imgname = f"{id:04d}.jpg"
+                src_path = os.path.join(origin_image_dir, imgname)
+                dst_path = os.path.join(target_path+'/images', imgname)
+                shutil.copy2(src_path, dst_path)
                 id = item['image_id']
                 content = ''                    
-                    
+
             category_id = str(item['category_id'])
-            x_center = "{:.6f}".format((item['bbox'][0]*2 + item['bbox'][2])/2.0)
-            y_center = "{:.6f}".format((item['bbox'][1]*2 + item['bbox'][3])/2.0)
-            width = str(item['bbox'][1])
-            height = str(item['bbox'][3])
+            x_center = "{:.6f}".format(((item['bbox'][0]*2 + item['bbox'][2])/2.0) / 1024)
+            y_center = "{:.6f}".format(((item['bbox'][1]*2 + item['bbox'][3])/2.0) / 1024)
+            width = "{:.6f}".format(item['bbox'][2] / 1024)
+            height = "{:.6f}".format(item['bbox'][3] / 1024)
             data = category_id + " " + x_center + " " + y_center + " " + width + " " + height + "\n"
             content += data
-        
+    
         filename = f"{id:04d}.txt"
-        output_path = os.path.join(output_dir, filename)
+        output_path = os.path.join(target_path+'/labels', filename)
         with open(output_path, 'w') as ff:
             print(output_path)
             ff.write(content)
+        imgname = f"{id:04d}.jpg"
+        src_path = os.path.join(origin_image_dir, imgname)
+        dst_path = os.path.join(target_path+'/images', imgname)
+        shutil.copy2(src_path, dst_path)
 
-# 사용 예시
-coco_json_path = './datasets/origin_dataset/train.json'
-output_dir = './datasets/yolo_form_dataset/labels/train'
-os.makedirs(output_dir, exist_ok=True)
-convert_coco_to_yolo(coco_json_path, output_dir)
+
+
+
+dataset_maker(train_fold_json_path, train_output_dir)
+dataset_maker(val_fold_json_path, val_output_dir)
+
+        
+    
+        
