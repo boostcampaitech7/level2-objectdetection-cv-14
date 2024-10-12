08722bbc62bb8a4a7dbc380a31e604f3b51e93a5
oweixx <lowzxx@naver.com>
2024-10-13 01:51:14 +0900

    [#26] Feature : Update swin_v9 (Add Albu & Resize multiscale)

--- mmdetection/custom_configs/swin_L/schedule.py
+++ mmdetection/custom_configs/swin_L/schedule.py
@@ -1,8 +1,9 @@
+# optimizer
 optimizer_config = dict(grad_clip=None)
 
 optimizer = dict(
     type='AdamW',
-    lr=0.00005,
+    lr=0.000025,
     betas=(0.9, 0.999),
     weight_decay=0.05,
     paramwise_cfg=dict(
@@ -14,13 +15,15 @@ optimizer = dict(
         )
     )
 
+# learning policy
 lr_config = dict(
-    policy='step',
+    policy='CosineRestart',
     warmup='linear',
-    warmup_iters=500,
+    warmup_iters=1099,
     warmup_ratio=0.001,
-    step=[2, 5, 8, 11],
-    gamma=0.5
-)
-
+    periods=[5495*4, 5495*4, 6594*4, 8792*4, 8792*4],
+    restart_weights=[1, 0.85, 0.75, 0.7, 0.6],
+    by_epoch=False,
+    min_lr=5e-6
+    )
 runner = dict(type='EpochBasedRunner', max_epochs=36)
