412bab63f3228ef78838a82a5486025c1fee3f41
kimgeonsu <kundoo0412@gmail.com>
2024-10-15 10:17:08 +0900

    [#35] Feature : add train option

--- yolo/train.py
+++ yolo/train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n.pt")
+model = YOLO("yolo11l.pt")
 
 # Train the model
 train_results = model.train(
@@ -9,6 +9,10 @@ train_results = model.train(
     epochs=100,  # number of training epochs
     imgsz=1024,  # training image size
     device='cuda',  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
+    # multi_scale=True,
+    patience=7,
+    batch=8,
+    iou=0.6,
 )
 
 # Evaluate model performance on the validation set
