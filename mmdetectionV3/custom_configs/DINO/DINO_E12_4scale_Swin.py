d8e8732ca8747b86a592dafbfc495434301f83bb
lkl4502 <lkl4502@naver.com>
2024-10-16 01:35:38 +0900

    [#42] Feature : Create DINO_E12_4scale_Swin.py

---
+++ mmdetectionV3/custom_configs/DINO/DINO_E12_4scale_Swin.py
@@ -0,0 +1,31 @@
+_base_ = "./Base_DINO_E12.py"
+
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth'  # noqa
+
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='SwinTransformer',
+        pretrain_img_size=384,
+        embed_dims=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        window_size=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        # Please only add indices that would be used
+        # in FPN, otherwise some parameter will not be used
+        with_cp=True,
+        convert_weights=True,
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+    neck=dict(in_channels=[192, 384, 768, 1536])
+    )
+
+# dino-4scale_r50_8xb2-12_coco를 사용했다는 것을 명식적으로 표현
+load_from = "https://download.openmmlab.com/mmdetection/v3.0/dino/dino-4scale_r50_8xb2-12e_coco/dino-4scale_r50_8xb2-12e_coco_20221202_182705-55b2bba2.pth"
