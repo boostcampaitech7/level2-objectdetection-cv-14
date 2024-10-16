bff03515cc407ae3dee95c7e54577e19c0f345e8
lkl4502 <lkl4502@naver.com>
2024-10-16 16:10:19 +0900

    [#42] Feature : TTA 적용 DINO 파일 생성

---
+++ mmdetectionV3/custom_configs/DINO/DINO_E12_4scale_Swin_tta.py
@@ -0,0 +1,31 @@
+_base_ = "./DINO_E12_4scale_Swin.py"
+
+img_scales = [(512,512), (640,640), (768,768), (896, 896), (1024, 1024)]
+
+tta_model = dict(
+    type='DetTTAModel',
+    tta_cfg=dict(nms=dict(
+                   type='nms',
+                   iou_threshold=0.5),
+                   max_per_img=100))
+
+tta_pipeline = [
+    dict(type='LoadImageFromFile',
+         backend_args=None),
+    dict(
+        type='TestTimeAug',
+        transforms=[[
+            dict(type='Resize', scale=s, keep_ratio=True) for s in img_scales
+        ], [
+            dict(type='RandomFlip', prob=1.),
+            dict(type='RandomFlip', prob=0.)
+        ], [
+            dict(type='PhotoMetricDistortion'),
+            dict(type='AddGaussianNoise', mean=0, std=0.01)
+        ], [
+            dict(
+               type='PackDetInputs',
+               meta_keys=('img_id', 'img_path', 'ori_shape',
+                       'img_shape', 'scale_factor', 'flip',
+                       'flip_direction'))
+       ]])]
