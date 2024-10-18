c73364a8554b4a0e345517591c94394ec0cb3a9c
lkl4502 <lkl4502@naver.com>
2024-10-18 11:21:38 +0900

    [#42] Feature : DINO 폴드 별 config 파일 추가

---
+++ mmdetectionV3/custom_configs/DINO/DINO_E15_5scale_Swin_2fold.py
@@ -0,0 +1,34 @@
+_base_ = "./DINO_E15_5scale_Swin.py"
+
+data_root = "../../../dataset/"
+
+train_dataloader = dict(
+    dataset=dict(
+        ann_file='sgkf_5_14/train_2fold.json'
+    )
+)
+
+val_dataloader = dict(
+    dataset=dict(
+        ann_file='sgkf_5_14/val_2fold.json'
+    )
+)
+
+val_evaluator = dict(
+    ann_file=data_root + 'sgkf_5_14/val_2fold.json',
+)
+
+vis_backends = [
+    dict(type='LocalVisBackend'),
+    dict(type='WandbVisBackend',
+         init_kwargs={
+             'project' : 'Recycle-Object-Detection',
+             'group' : 'DINO-5scale-Swin',
+             'name' : 'Base-DINO-5Scale-Swin-Improve-2fold',
+             'entity' : 'cv14_',
+             'tags' : ['Swin384', '15Epoch', '5scale']
+         })
+]
+
+visualizer = dict(
+    type='DetLocalVisualizer', vis_backends=vis_backends, name='visualizer')
