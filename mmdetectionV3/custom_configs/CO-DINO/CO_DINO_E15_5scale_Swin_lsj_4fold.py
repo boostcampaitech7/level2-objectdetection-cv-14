c591ec2e44129489c3dabf111f3cb1d64462f3c0
lkl4502 <lkl4502@naver.com>
2024-10-20 23:59:56 +0900

    [#42] Feature : Create config file to obtain the best model each fold

---
+++ mmdetectionV3/custom_configs/CO-DINO/CO_DINO_E15_5scale_Swin_lsj_4fold.py
@@ -0,0 +1,31 @@
+_base_ = "./CO_DINO_E15_5scale_Swin_lsj.py"
+
+train_dataloader = dict(
+    dataset=dict(
+        ann_file='sgkf_5_14/train_4fold.json'
+    )
+)
+
+val_dataloader = dict(
+    dataset=dict(
+        ann_file='sgkf_5_14/val_4fold.json'
+    )
+)
+
+val_evaluator = dict(
+    ann_file=_base_.data_root + 'sgkf_5_14/val_4fold.json',
+)
+
+vis_backends = [
+    dict(type='LocalVisBackend'),
+    dict(type='WandbVisBackend',
+         init_kwargs={
+            'project' : 'Recycle-Object-Detection',
+            'group' : 'CO-DINO-5scale-Swin',
+            'name' : 'CO-DINO-5scale-Swin-4fold',
+            'entity' : 'cv14_',
+            'tags' : ['Swin384', '15Epoch', '5scale', 'LSJ']
+         })]
+
+visualizer = dict(
+    type='DetLocalVisualizer', vis_backends=vis_backends, name='visualizer')
