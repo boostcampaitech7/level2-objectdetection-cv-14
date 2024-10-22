fa2c867bef07146c519411b6c3f4741c3f0f3569
kimgeonsu <kundoo0412@gmail.com>
2024-10-22 10:28:40 +0900

    [#59] Feature : ensemble little upgrade

--- utils/Ensemble.py
+++ utils/Ensemble.py
@@ -4,8 +4,10 @@ import numpy as np
 from tqdm import tqdm
 from ensemble_boxes import *
 
-def main() : 
-    csv_files = [ ]  # Ensemble을 진행할 csv file 경로 입력
+
+def main():
+    csv_files = [os.path.join('./target', f)
+                 for f in os.listdir('./target') if f.endswith('.csv')]
     combined_df = [pd.read_csv(f) for f in csv_files]
 
     image_ids = combined_df[0]['image_id'].tolist()
@@ -16,23 +18,24 @@ def main() :
 
     img_width, img_height = 1024, 1024
 
-    for i, image_id in tqdm(enumerate(image_ids), total=len(image_ids)) :
+    for i, image_id in tqdm(enumerate(image_ids), total=len(image_ids)):
         prediction_string = ''
         boxes_list = []
         scores_list = []
         labels_list = []
 
-        for df in combined_df :
-            predict_string = df[df['image_id'] == image_id]['PredictionString'].tolist()[0]
+        for df in combined_df:
+            predict_string = df[df['image_id'] == image_id]['PredictionString'].tolist()[
+                0]
             predict_list = str(predict_string).split()
 
-            if len(predict_list) <= 1 :
+            if len(predict_list) <= 1:
                 continue
 
             predict_list = np.reshape(predict_list, (-1, 6))
             box_list = []
 
-            for box in predict_list[:, 2:6].tolist() : 
+            for box in predict_list[:, 2:6].tolist():
                 box[0] = float(box[0]) / img_width
                 box[1] = float(box[1]) / img_height
                 box[2] = float(box[2]) / img_width
@@ -45,25 +48,17 @@ def main() :
 
         # Ensemble 방법 선택
         iou_thr = 0.5
-        if len(boxes_list) :
-            
-            # NMS
-            #boxes, scores, labels = nms(boxes_list, scores_list, labels_list, iou_thr=iou_thr)
-            
-            # Soft_NMS
-            #boxes, scores, labels = soft_nms(boxes_list, scores_list, labels_list, iou_thr=iou_thr)
+        if len(boxes_list):
+            # boxes, scores, labels = nms(boxes_list, scores_list, labels_list, iou_thr=iou_thr) # NMS
+            # boxes, scores, labels = soft_nms(boxes_list, scores_list, labels_list, iou_thr=iou_thr) # Soft_NMS
+            # boxes, scores, labels = weighted_boxes_fusion(boxes_list, scores_list, labels_list, iou_thr=iou_thr) # WBF
+            boxes, scores, labels = weighted_boxes_fusion(
+                boxes_list, scores_list, labels_list, iou_thr=iou_thr, conf_type='box_and_model_avg')  # WBF with box_and_model_avg
+            # boxes, scores, labels = non_maximum_weighted(boxes_list, scores_list, labels_list, iou_thr=iou_thr) # NMW
 
-            # WBF
-            #boxes, scores, labels = weighted_boxes_fusion(boxes_list, scores_list, labels_list, iou_thr=iou_thr)
-
-            # WBF with box_and_model_avg
-            boxes, scores, labels = weighted_boxes_fusion(boxes_list, scores_list, labels_list, iou_thr=iou_thr, conf_type='box_and_model_avg')
-            
-            # NMW
-            #boxes, scores, labels = non_maximum_weighted(boxes_list, scores_list, labels_list, iou_thr=iou_thr)
-            
-            for box, score, label in zip(boxes, scores, labels) :
-                prediction_string += str(int(label)) + ' ' + str(score) + ' ' + str(box[0] * img_width) + ' ' + str(box[1] * img_height) + ' ' + str(box[2] * img_width) + ' ' + str(box[3] * img_height) + ' '
+            for box, score, label in zip(boxes, scores, labels):
+                prediction_string += str(int(label)) + ' ' + str(score) + ' ' + str(box[0] * img_width) + ' ' + str(
+                    box[1] * img_height) + ' ' + str(box[2] * img_width) + ' ' + str(box[3] * img_height) + ' '
 
         prediction_strings.append(prediction_string)
         file_names.append(image_id)
@@ -73,9 +68,21 @@ def main() :
     submission['image_id'] = file_names
 
     # csv file 경로 입력
-    save_dir = ''
+    save_dir = './output'
 
-    submission.to_csv(os.path.join(save_dir, 'ensemble.csv'), index=False)
+    # 결과를 저장할 파일 이름 생성
+    base_filename = 'ensemble.csv'
+    counter = 1
+    output_filename = base_filename
 
-if __name__ == '__main__' :
+    while os.path.exists(os.path.join(save_dir, output_filename)):
+        output_filename = f'ensemble_{counter}.csv'
+        counter += 1
+
+    # 결과 저장
+    submission.to_csv(os.path.join(save_dir, output_filename), index=False)
+    print(f'결과가 {output_filename}로 저장되었습니다.')
+
+
+if __name__ == '__main__':
     main()
